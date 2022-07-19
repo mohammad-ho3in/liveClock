c50b069a2ba5f14c853214359d077e0624f4d060
mohammad hossein rashidi <mhr.pyt@gmail.com>
2022-07-20 01:43:44 +0430

    instant display of the clock on the web page with jQuery

---
+++ README.md
@@ -0,0 +1,16 @@
+<a><img src="https://img.shields.io/badge/JavaScript-jQuery-yellow" alt="Total Downloads"></a>
+
+<h1>Instant display of the clock on the web page
+
+## Required Component
+- jQuery
+- liveTime.js
+
+## USAGE
+1. Set a name for the element class
+2. insert jquery
+3. insert file liveTime.js
+4. used function
+```javascript
+liveTime('class_name');
+```
---
+++ src/liveTime.js
@@ -0,0 +1,32 @@
+
+function liveTime(className) {
+    $(document).ready(function () {
+        clockUpdate();
+        setInterval(clockUpdate, 1000);
+    })
+    function clockUpdate() {
+        var date = new Date();
+        function addZero(x) {
+            if (x < 10) {
+                return x = '0' + x;
+            } else {
+                return x;
+            }
+        }
+        function twelveHour(x) {
+            if (x == 0) {
+                return x = 12;
+            } else {
+                return x;
+            }
+        }
+        var h = addZero(twelveHour(date.getHours()));
+        var m = addZero(date.getMinutes());
+        var s = addZero(date.getSeconds());
+        var elementType = $('.' + className)[0]['nodeName'];
+        if (elementType == 'INPUT')
+            $('.' + className).val(h + ':' + m + ':' + s)
+        else
+            $('.' + className).text(h + ':' + m + ':' + s)
+    }
+}
